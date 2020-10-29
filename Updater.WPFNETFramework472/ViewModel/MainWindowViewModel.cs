ab210c36b3d411d62a11366ba3f29bf6177e2e0a
washington.santos <washingtonsilvasan@gmail.com>
2020-10-28 22:17:38 -0300

    Ajustes

--- Updater.WPFNETFramework472/ViewModel/MainWindowViewModel.cs
+++ Updater.WPFNETFramework472/ViewModel/MainWindowViewModel.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,22 +16,23 @@ namespace Updater.WPFNETFramework472.ViewModel
 {
     public class MainWindowViewModel : BindableBase
     {
-        private static string url = @"http://extranet.mgcontecnica.com.br:8000/download/RegistroPonto.exe";
-        private static string caminhoTemporario = $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\Downloads\pontoVirtualTemp";
+        private static string url = @"http://extranet.mgcontecnica.com.br:8000/download/RegistroPonto.zip";
+        private static string caminhoTemporario = $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\Downloads\pontoVirtualTemp\download";
         private static string caminhoTemporarioBKP = $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\Downloads\pontoVirtualTemp\bkp";
         private static string _pathPadrao = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Programs\Registro Ponto";
-        
+        private static string _nameFileZipToDownload = "RegistroPonto.zip";
+
         public MainWindowViewModel()
         {
-            Infos = new ObservableCollection<Info>();
-            CriarCaminhoTemp();
-            ExecutarDownloadCommand();
+            Infos = new ObservableCollection<string>();
+
+            Processar();
         }
 
         #region [COMMANDS]
         private DelegateCommand _downloadCommand;
         public DelegateCommand DownloadCommand =>
-            _downloadCommand ?? (_downloadCommand = new DelegateCommand(ExecutarDownloadCommand));
+            _downloadCommand ?? (_downloadCommand = new DelegateCommand(Processar));
         #endregion
 
         #region [PROPERTIES]
@@ -46,8 +49,16 @@ namespace Updater.WPFNETFramework472.ViewModel
             get { return _currentProgress; }
             set { SetProperty(ref _currentProgress, value); }
         }
-        private ObservableCollection<Info> _infos;
-        public ObservableCollection<Info> Infos 
+
+        private int _currentProgressInstall;
+        public int CurrentProgressInstall
+        {
+            get { return _currentProgressInstall; }
+            set { SetProperty(ref _currentProgressInstall, value); }
+        }
+
+        private ObservableCollection<string> _infos;
+        public ObservableCollection<string> Infos 
         {
             get { return _infos; }
             set { SetProperty(ref _infos, value); }
@@ -59,25 +70,36 @@ namespace Updater.WPFNETFramework472.ViewModel
         {
             try
             {
-                InsertObservableInfos(1, "Criando Caminho Temporário OK", "information");
+                Infos.Add("Criando Caminho Temporário OK");
 
                 if (!Directory.Exists(caminhoTemporario))
                 {
                     Directory.CreateDirectory(caminhoTemporario);
-                    UpdateObservableInfos(10, "Diretório Caminho Temporário Criado");
+                    Infos.Add("Diretório Temporário Criado");
+                }
+
+                if (!Directory.Exists(caminhoTemporarioBKP))
+                {
+                    Directory.CreateDirectory(caminhoTemporarioBKP);
+                    Infos.Add("Diretório Temporário BKP Criado");
                 }
             }
             catch (Exception ex)
             {
-                UpdateObservableInfos(1, "Falha ao criar Caminho Temporário");
-                UpdateObservableInfos(10, "Processo de Atualização Cancelado");
+                Infos.Add("Falha ao criar Diretório Temporário Criado");
+                Infos.Add("Processo de Atualização Cancelado");
+                Roolback();
             }
-
         }
 
-        private async void ExecutarDownloadCommand()
+        /// <summary>
+        /// Inicia efetuando Download
+        /// </summary>
+        private async void Processar()
         {
-            Infos.Add(new Info { Id = 2, Mensagem = "Efetuando Download" });
+            CriarCaminhoTemp();
+
+            Infos.Add("Efetuando Download");
             //construct Progress<T>, passing ReportProgress as the Action<T> 
             var progressIndicator = new Progress<ProgressEventArgsEx>(ReportProgress);
             await DownloadStraingAsyncronous(url, progressIndicator);
@@ -94,16 +116,17 @@ namespace Updater.WPFNETFramework472.ViewModel
                     client.DownloadProgressChanged += client_DownloadProgressChanged;
                     client.DownloadFileCompleted += client_DownloadFileCompleted;
                     Console.WriteLine(@"Downloading file:");
-                    client.DownloadFileAsync(ur, caminhoTemporario + "\\RegistroPonto.exe");
+                    client.DownloadFileAsync(ur, caminhoTemporario + "\\RegistroPonto.zip");
                     return File.Exists(caminhoTemporario).ToString();
                 }
             }
             catch (Exception ex)
             {
-                UpdateObservableInfos(10, "Falha no Download, Atualização Cancelada");
+                Infos.Add("Falha no Download, Processo de Atualização Cancelado");
+                Roolback();
             }
 
-            UpdateObservableInfos(2, "Download Concluído");
+            Infos.Add("Download concluído");
 
             return string.Empty;
         }
@@ -122,16 +145,48 @@ namespace Updater.WPFNETFramework472.ViewModel
         {
             try
             {
+                CurrentProgressInstall = 20;
+                
                 bool bkpOK = BackupLastVersion();
 
-                if (bkpOK)
-                    StartInstalation();
-                else
-                    RemoveBackLastVersion();
+                if (!bkpOK) Roolback();
+
+                Infos.Add("Instalando");
+
+                CurrentProgressInstall = 40;
+                bool descompactou = Descompacta();
+
+                if (!descompactou) Roolback();
+
+                CurrentProgressInstall = 60;
+
+                RemoveDownloadZIP();
+                RemoveLastVersion();
+
+                CurrentProgressInstall = 80;
+
+                bool filesCopy = CopyFilesToDirectoryDefault();
+
+                if (!filesCopy)
+                {
+                    RetornaVersaoAnterior();
+                    Roolback();
+                }
+                RemoveBackLastVersion();
+                RemoveDownload();
+
+                CurrentProgressInstall = 100;
+
+                Infos.Add("Atualizado com sucesso!");
+
+                var executalvelStart = _pathPadrao + "\\PontoVirtual.Presentation.Wpf.exe";
+                Process.Start(executalvelStart);
+
+                App.Current.Shutdown();
             }
             catch (Exception ex)
             {
-                UpdateObservableInfos(10, "Atualização Cancelada");
+                Infos.Add("Falha na instalação, Atualização Cancelada");
             }
         }
 
@@ -140,14 +195,11 @@ namespace Updater.WPFNETFramework472.ViewModel
             Percentage = args.Text + " " + args.Percentage;
         }
 
-        private bool BackupLastVersion()
+        private bool RemoveLastVersion()
         {
             try
             {
-                Infos.Add(new Info { Id = 3, Mensagem = "Iniciando BKP versão anterior"});
-
-                if (!Directory.Exists(caminhoTemporarioBKP))
-                    Directory.CreateDirectory(caminhoTemporarioBKP);
+                Infos.Add("Removendo versão anterior...");
 
                 var dir = new DirectoryInfo(_pathPadrao);
 
@@ -157,7 +209,76 @@ namespace Updater.WPFNETFramework472.ViewModel
 
                 foreach (FileInfo file in files)
                 {
-                    UpdateObservableInfos(3, "Efetuando BKP versão anterior " + (1 / countFiles * 100) + "%");
+                    file.Delete();
+                }
+            }
+            catch (Exception ex)
+            {
+                Infos.Add("Falha na Instalação, Atualização Cancelada");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CopyFilesToDirectoryDefault()
+        {
+            try
+            {
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = new DirectoryInfo(caminhoTemporario).GetFiles();
+                int countFiles = files.Length;
+
+                foreach (FileInfo file in files)
+                {
+                    string _pathDefault = Path.Combine(_pathPadrao, file.Name);
+                    file.CopyTo(_pathDefault, false);
+                }
+            }
+            catch (Exception)
+            {
+                Infos.Add("Falha na Instação, Atualização Cancelada");
+            }
+
+            return true;
+        }
+       
+        private bool RetornaVersaoAnterior()
+        {
+            try
+            {
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = new DirectoryInfo(caminhoTemporarioBKP).GetFiles();
+                int countFiles = files.Length;
+
+                foreach (FileInfo file in files)
+                {
+                    string _pathDefault = Path.Combine(_pathPadrao, file.Name);
+                    file.CopyTo(_pathDefault, false);
+                }
+            }
+            catch (Exception)
+            {
+                Infos.Add("Falha na restauração da versão anterior, Atualização Cancelada");
+            }
+
+            return true;
+        }
+
+        private bool BackupLastVersion()
+        {
+            try
+            {
+                Infos.Add("Iniciando BKP versão anterior");
+
+                var dir = new DirectoryInfo(_pathPadrao);
+
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+                int countFiles = files.Length;
+
+                foreach (FileInfo file in files)
+                {
                     string tempPath = Path.Combine(caminhoTemporarioBKP, file.Name);
                     file.CopyTo(tempPath, false);
                 }
@@ -165,55 +286,119 @@ namespace Updater.WPFNETFramework472.ViewModel
             }
             catch (Exception ex)
             {
-                UpdateObservableInfos(3, "Falha no BKP da versão anterior, Atualização Cancelada");
+                Infos.Add("Falha no BKP versão anterior, Atualização Cancelada");
                 return false;
             }
 
-            UpdateObservableInfos(3, "BKP versão anterior concluído");
-            
+            Infos.Add("BKP versão anterior concluído");
+
             return true;
         }
         
         private void RemoveBackLastVersion()
         {
-            var dir = new DirectoryInfo(caminhoTemporarioBKP);
-
-            // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files)
+            try
             {
-                file.Delete();
+                var dir = new DirectoryInfo(caminhoTemporarioBKP);
+
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    file.Delete();
+                }
+
+                if (Directory.Exists(caminhoTemporarioBKP))
+                    Directory.Delete(caminhoTemporarioBKP);
+            }
+            catch (Exception)
+            {
+                Infos.Add("Falha na  remoção do BKP da versão anterior, Atualização Cancelada");
+            }
+
+            Infos.Add("BKP removido...");
+        }
+
+        private void RemoveDownloadZIP()
+        {
+            try
+            {
+                File.Exists(caminhoTemporario +"\\"+ _nameFileZipToDownload);
+                  File.Delete(caminhoTemporario + "\\" +  _nameFileZipToDownload);
+            }
+            catch (Exception)
+            {
+                Infos.Add("Falha no BKP versão anterior, Atualização Cancelada");
+            }
+
+            Infos.Add("Download removido...");
+        }
+
+        private void RemoveDownload()
+        {
+            try
+            {
+                var dir = new DirectoryInfo(caminhoTemporario);
+
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    file.Delete();
+                }
+
+                if (Directory.Exists(caminhoTemporario))
+                    Directory.Delete(caminhoTemporario);
+            }
+            catch (Exception)
+            {
+                Infos.Add("Falha na remoção do download, Atualização Cancelada");
+            }
+
+            Infos.Add("Download removido...");
+        }
+
+        private bool Descompacta()
+        {
+            try
+            {
+                var gzipFileName = new DirectoryInfo(caminhoTemporario).GetFiles("RegistroPonto.zip", SearchOption.TopDirectoryOnly).FirstOrDefault();
+
+                string decompressedFileName = $@"{caminhoTemporario}\{gzipFileName.Name}";
+
+                using (ZipArchive archive = ZipFile.OpenRead(decompressedFileName))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                            // Gets the full path to ensure that relative segments are removed.
+                            string destinationPath = Path.GetFullPath(Path.Combine(caminhoTemporario, entry.FullName));
+
+                            // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                            // are case-insensitive.
+                            if (destinationPath.StartsWith(caminhoTemporario, StringComparison.Ordinal))
+                                entry.ExtractToFile(destinationPath);
+                    }
+                }
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Infos.Add("Falha ao Descompactar, Atualização Cancelada");
+                return false;
             }
         }
 
-        private bool StartInstalation()
+        private bool Roolback()
         {
-            InsertObservableInfos(4, "Iniciando Instalação", "information");
-
-            //Extrair ZIP
-
-            //Copiar Para a Pasta
+            CurrentProgressInstall = 80;
+            RemoveBackLastVersion();
+            CurrentProgressInstall = 50;
+            RemoveDownload();
+            CurrentProgressInstall = 30;
 
             return true;
         }
-
-        private void InsertObservableInfos(int id, string novaMensagem, string icon = null)
-        {
-            Infos.Add(new Info { Id = id, Mensagem = novaMensagem});
-        }
-                private void UpdateObservableInfos(int id, string novaMensagem)
-        {
-            var oldItem = Infos.FirstOrDefault(i => i.Id == id);
-            var oldIndex = Infos.IndexOf(oldItem);
-            Infos[oldIndex].Mensagem = novaMensagem;
-        }
         #endregion
     }
-
-    public class Info {
-
-        public int Id { get; set; }
-        public string Mensagem { get; set; }
-    }
-
 }
